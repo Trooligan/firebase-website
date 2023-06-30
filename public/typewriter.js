e90e367b8613762c3b471ed2dd4e6ca546787b5b
Trooligan <103884458+Trooligan@users.noreply.github.com>
2023-06-30 16:23:18 +0300

    Updates to frontend

---
+++ public/typewriter.js
@@ -0,0 +1,38 @@
+var app = document.getElementById("test1");
+
+var typewriterSkills1 = new Typewriter(app, {
+  delay: 20,
+  deleteSpeed: 30,
+  loop: true,
+});
+
+typewriterSkills1
+  .typeString("Docker")
+  .pauseFor(1500)
+  .deleteChars(5)
+  .typeString("ata Engineering")
+  .pauseFor(1500)
+  .deleteAll()
+  .typeString("Traefik, Node.js, REST/API")
+  .pauseFor(1500)
+  .deleteAll()
+  .typeString("Python, C#, C++")
+  .pauseFor(1500)
+  .deleteAll()
+  .start();
+
+var frontpageHeader1 = document.getElementById("frontpage-header-1");
+
+var typewriterFrontpage1 = new Typewriter(frontpageHeader1, {
+  delay: 20,
+  deleteSpeed: 30,
+  loop: false,
+});
+
+typewriterFrontpage1
+  .typeString("A place where the matrix")
+  .deleteChars(6)
+  .typeString("<span class='Pulse'>MATRIX</span> becomes a reality")
+  .deleteChars(7)
+  .typeString("<span class='Pulse'>REALITY</span>")
+  .start();
